696d263a84bd218268a0ba1841e309c2d5e1de24
Quynh Nguyen <mr.quynh@gmail.com>
2018-04-11 04:37:16 +0700

    Initialize

---
+++ FleckLog.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+
+namespace Fleck
+{
+    public class FleckLog
+    {
+		static ILogger _Logger = null;
+
+		internal static ILogger Logger
+		{
+			get
+			{
+				return FleckLog._Logger ?? (FleckLog._Logger = Fleck.Logger.CreateLogger<WebSocketServer>());
+			}
+		}
+
+		public static void Trace(string message, Exception ex = null)
+		{
+			FleckLog.Logger.LogTrace(ex, message);
+		}
+
+		public static void Info(string message, Exception ex = null)
+		{
+			FleckLog.Logger.LogInformation(ex, message);
+		}
+
+		public static void Warn(string message, Exception ex = null)
+        {
+			FleckLog.Logger.LogWarning(ex, message);
+		}
+
+		public static void Debug(string message, Exception ex = null)
+		{
+			FleckLog.Logger.LogDebug(ex, message);
+		}
+
+		public static void Error(string message, Exception ex = null)
+        {
+			FleckLog.Logger.LogError(ex, message);
+		}
+    }
+
+	#region Logger
+	public static class Logger
+	{
+		static ILoggerFactory LoggerFactory;
+
+		/// <summary>
+		/// Assigns a logger factory
+		/// </summary>
+		/// <param name="loggerFactory"></param>
+		public static void AssignLoggerFactory(ILoggerFactory loggerFactory)
+		{
+			if (Logger.LoggerFactory == null && loggerFactory != null)
+				Logger.LoggerFactory = loggerFactory;
+		}
+
+		/// <summary>
+		/// Gets a logger factory
+		/// </summary>
+		/// <returns></returns>
+		public static ILoggerFactory GetLoggerFactory()
+		{
+			return Logger.LoggerFactory ?? new NullLoggerFactory();
+		}
+
+		/// <summary>
+		/// Creates a logger
+		/// </summary>
+		/// <param name="type"></param>
+		/// <returns></returns>
+		public static ILogger CreateLogger(Type type)
+		{
+			return Logger.GetLoggerFactory().CreateLogger(type);
+		}
+
+		/// <summary>
+		/// Creates a logger
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
+		public static ILogger CreateLogger<T>()
+		{
+			return Logger.CreateLogger(typeof(T));
+		}
+	}
+	#endregion
+
+	#region NullLogger
+
+	public class NullLoggerFactory : ILoggerFactory
+	{
+		public void AddProvider(ILoggerProvider provider) { }
+
+		public ILogger CreateLogger(string categoryName)
+		{
+			return NullLogger.Instance;
+		}
+
+		public void Dispose() { }
+	}
+
+	public class NullLogger : ILogger
+	{
+		internal static NullLogger Instance = new NullLogger();
+
+		private NullLogger() { }
+
+		public IDisposable BeginScope<TState>(TState state) { return null; }
+
+		public bool IsEnabled(LogLevel logLevel)
+		{
+			return false;
+		}
+
+		public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter) { }
+	}
+	#endregion
+
+}
---
+++ WebSocketServer.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
+using System.Security.Authentication;
+
+namespace Fleck
+{
+	public class WebSocketServer : IWebSocketServer
+	{
+		string _scheme;
+		IPAddress _locationIP;
+		Action<IWebSocketConnection> _config;
+
+		public WebSocketServer(string location, Microsoft.Extensions.Logging.ILoggerFactory loggerFactory = null)
+		{
+			Logger.AssignLoggerFactory(loggerFactory);
+			var uri = new Uri(location);
+			this.Port = uri.Port;
+			this.Location = location;
+			this._locationIP = ParseIPAddress(uri);
+			this._scheme = uri.Scheme;
+			var socket = new Socket(_locationIP.AddressFamily, SocketType.Stream, ProtocolType.IP);
+			this.ListenerSocket = new SocketWrapper(socket);
+			this.SupportedSubProtocols = new string[0];
+		}
+
+		public ISocket ListenerSocket { get; set; }
+		public string Location { get; private set; }
+		public int Port { get; private set; }
+		public X509Certificate2 Certificate { get; set; }
+		public SslProtocols EnabledSslProtocols { get; set; }
+		public IEnumerable<string> SupportedSubProtocols { get; set; }
+		public bool RestartAfterListenError { get; set; }
+
+		public bool IsSecure
+		{
+			get { return _scheme == "wss" && Certificate != null; }
+		}
+
+		public void Dispose()
+		{
+			ListenerSocket.Dispose();
+		}
+
+		private IPAddress ParseIPAddress(Uri uri)
+		{
+			string ipStr = uri.Host;
+
+			if (ipStr == "0.0.0.0")
+			{
+				return IPAddress.Any;
+			}
+			else if (ipStr == "[0000:0000:0000:0000:0000:0000:0000:0000]")
+			{
+				return IPAddress.IPv6Any;
+			}
+			else
+			{
+				try
+				{
+					return IPAddress.Parse(ipStr);
+				}
+				catch (Exception ex)
+				{
+					throw new FormatException("Failed to parse the IP address part of the location. Please make sure you specify a valid IP address. Use 0.0.0.0 or [::] to listen on all interfaces.", ex);
+				}
+			}
+		}
+
+		public void Start(Action<IWebSocketConnection> config)
+		{
+			var ipLocal = new IPEndPoint(_locationIP, Port);
+			ListenerSocket.Bind(ipLocal);
+			ListenerSocket.Listen(1000);
+			Port = ((IPEndPoint)ListenerSocket.LocalEndPoint).Port;
+			if (_scheme == "wss")
+			{
+				if (Certificate == null)
+				{
+					FleckLog.Error("Scheme cannot be 'wss' without a Certificate");
+					return;
+				}
+
+				if (EnabledSslProtocols == SslProtocols.None)
+				{
+					EnabledSslProtocols = SslProtocols.Tls;
+					FleckLog.Trace("Using default TLS 1.0 security protocol.");
+				}
+			}
+			ListenForClients();
+			_config = config;
+		}
+
+		private void ListenForClients()
+		{
+			ListenerSocket.Accept(OnClientConnect, e =>
+			{
+				FleckLog.Error("Listener socket is closed", e);
+				if (RestartAfterListenError)
+				{
+					FleckLog.Info("Listener socket restarting");
+					try
+					{
+						ListenerSocket.Dispose();
+						var socket = new Socket(_locationIP.AddressFamily, SocketType.Stream, ProtocolType.IP);
+						ListenerSocket = new SocketWrapper(socket);
+						Start(_config);
+						FleckLog.Info("Listener socket restarted");
+					}
+					catch (Exception ex)
+					{
+						FleckLog.Error("Listener could not be restarted", ex);
+					}
+				}
+			});
+		}
+
+		private void OnClientConnect(ISocket clientSocket)
+		{
+			if (clientSocket == null) return; // socket closed
+
+			FleckLog.Trace($"Client connected from {clientSocket.RemoteIpAddress}:{clientSocket.RemotePort}");
+			ListenForClients();
+
+			WebSocketConnection connection = null;
+
+			connection = new WebSocketConnection(
+				clientSocket,
+				_config,
+				bytes => RequestParser.Parse(bytes, _scheme),
+				r => HandlerFactory.BuildHandler(r,
+					s => connection.OnMessage(s),
+					connection.Close,
+					b => connection.OnBinary(b),
+					b => connection.OnPing(b),
+					b => connection.OnPong(b)),
+				s => SubProtocolNegotiator.Negotiate(SupportedSubProtocols, s));
+
+			if (IsSecure)
+			{
+				FleckLog.Debug("Attempting to secure connection...");
+				clientSocket.Authenticate(Certificate, EnabledSslProtocols, connection.StartReceiving, e => FleckLog.Error($"Cannot secure the connection: {e.Message}", e));
+			}
+			else
+			{
+				connection.StartReceiving();
+			}
+		}
+
+		public static void AssignLoggerFactory(Microsoft.Extensions.Logging.ILoggerFactory loggerFactory)
+		{
+			Logger.AssignLoggerFactory(loggerFactory);
+		}
+	}
+}
