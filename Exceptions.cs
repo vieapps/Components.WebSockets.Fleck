3884fa3c19f13f7584b5d160da4ef50c95e10928
Quynh Nguyen <mr.quynh@gmail.com>
2018-04-27 14:50:57 +0700

    Refactoring

--- Exceptions.cs
+++ Exceptions.cs
@@ -28,4 +28,24 @@ namespace Fleck
 
 		public SubProtocolNegotiationFailureException(string message, Exception innerException) : base(message, innerException) { }
 	}
+
+	public class WebSocketException : Exception
+	{
+		public WebSocketException(ushort statusCode) : base()
+		{
+			StatusCode = statusCode;
+		}
+
+		public WebSocketException(ushort statusCode, string message) : base(message)
+		{
+			StatusCode = statusCode;
+		}
+
+		public WebSocketException(ushort statusCode, string message, Exception innerException) : base(message, innerException)
+		{
+			StatusCode = statusCode;
+		}
+
+		public ushort StatusCode { get; private set; }
+	}
 }
